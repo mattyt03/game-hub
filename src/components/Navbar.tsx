1d6f311eba7e766f8bc784d636490d29ae560f6d
Matthew Tolea <matthewtolea@gmail.com>
2023-07-09 20:43:11 -0500

    built search input

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
+import SearchInput from './SearchInput'
 
 const Navbar = () => {
   return (
     // HStack aligns items horizontally
-    <HStack justifyContent={'space-between'}>
+    <HStack>
         <Image src={logo} boxSize={'60px'}/>
+        <SearchInput/>
         <ColorModeSwitch/>
     </HStack>
   )
